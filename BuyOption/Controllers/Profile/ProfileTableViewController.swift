f2f8588d3b6a01d9d130b69789c6c64d8ca860e6
ELEMBA ADI Eric <elembaadi@icloud.com>
2018-04-19 15:37:44 +0200

    Profile, Panier, Search storyboard done

--- BuyOption/Controllers/Profile/ProfileTableViewController.swift
+++ BuyOption/Controllers/Profile/ProfileTableViewController.swift
@@ -1,27 +1,28 @@
 //
-//  CheckoutTableViewController.swift
+//  ProfileTableViewController.swift
 //  BuyOption
 //
-//  Created by Eric ELEMBA ADI on 18/04/2018.
+//  Created by Eric ELEMBA ADI on 19/04/2018.
 //  Copyright © 2018 Eric ELEMBA ADI. All rights reserved.
 //
 
 import UIKit
 
-class CheckoutTableViewController: UITableViewController {
-    
-    struct Storyboard {
-        static let nbrOfItmesCell = "nbrOfItmesCell"
-        static let itemCell = "itemCell"
-        static let cartDetailCell = "cartDetailCell"
-        static let cartTotalCell = "cartTotalCell"
-        static let checkoutButtonCell = "checkoutButtonCell"
-    }
+class ProfileTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.estimatedRowHeight = tableView.rowHeight
-        tableView.rowHeight = UITableViewAutomaticDimension
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
